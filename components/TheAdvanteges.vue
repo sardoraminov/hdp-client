b28d908051f761314c16897e3c576aae3fdec7fb
sardoraminov <oddiydasturchi@gmail.com>
2022-06-04 02:00:21 +0500

    changes

--- components/TheAdvanteges.vue
+++ components/TheAdvanteges.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="advanteges" class="advantege-part overflow-hidden sm:px-20 px-4 pb-6">
+  <div
+    id="advanteges"
+    class="advantege-part overflow-hidden sm:px-20 px-4 pb-6"
+  >
     <div class="part-titles mt-10 text-center mb-8">
       <h1 class="title text-[30px] font-extrabold font-mont inline-block">
         Afzalliklar
@@ -47,7 +50,7 @@
           <p class="ml-3 font-bold">Darsliklar mavjudligi</p>
         </div>
         <div
-          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-4 px-6 py-4 pb-6 text-[18px]"
+          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-6 px-6 py-4 pb-6 text-[18px]"
         >
           <p>
             HDP darsliklari o’quv markazimizning tajribali ustozlari tomonidan
@@ -83,7 +86,37 @@
           </ul>
         </div>
       </section>
-      <section class="section overflow-hidden rounded mb-8">
+      <svg
+        width="249"
+        height="475"
+        viewBox="0 0 249 475"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute -left-24 top-96 -z-20"
+      >
+        <circle cx="49" cy="275" r="200" fill="#00B4D8" fill-opacity="0.12" />
+        <rect
+          x="73.1991"
+          width="100"
+          height="100"
+          transform="rotate(21.8384 73.1991 0)"
+          fill="url(#paint0_linear_108_32)"
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_108_32"
+            x1="123.199"
+            y1="-1.49012e-06"
+            x2="173.199"
+            y2="133"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#00B4D8" />
+            <stop offset="1" stop-color="#98EEFF" />
+          </linearGradient>
+        </defs>
+      </svg>
+      <section class="section relative overflow-hidden rounded mb-8">
         <div
           class="section-title rounded-t bg-primary text-xl px-6 py-5 text-white flex flex-row items-center"
         >
@@ -91,7 +124,7 @@
           <p class="ml-3 font-bold">Markazimiz joylashuvi qulayligi</p>
         </div>
         <div
-          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-4 px-6 py-4 pb-6 text-[18px]"
+          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-6 px-6 py-4 pb-6 text-[18px]"
         >
           <p>
             O’quv markazimizning bosh filiali Urganch shahrining markaziy
@@ -99,12 +132,12 @@
             borish-kelish imkoniyati ancha oson hisoblanadi. Shu sababli
             o’quvchilarimiz darsga o’z vaqtidan kech kelish holatlari kam
             kuzatiladi. Bundan tashqari, viloyatning Gurlan tumanida ham
-            markazimiz filiali faoliyat yuritib kelmoqda. Ularni pastda Google
-            Maps orqali qizil markerda ko'rishingiz mumkin 😊
+            markazimiz filiali faoliyat yuritib kelmoqda. Pastda esa bosh
+            filialimizning joylashuvini Google Maps orqali qizil markerda
+            ko'rishingiz mumkin 😊
           </p>
           <div class="maps">
-            <div class="map border-2 border-gray p-2">
-              <p class="title mb-2">Bosh filial</p>
+            <div class="map">
               <iframe
                 src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3144.7758044817074!2d60.61261601540251!3d41.56060769320162!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x41dfc9eba4e2c17f%3A0x785527c74348e063!2sHDP%20o&#39;quv%20markazi!5e1!3m2!1sen!2s!4v1654280907067!5m2!1sen!2s"
                 height="450"
@@ -115,19 +148,147 @@
                 class="w-full border-gray"
               ></iframe>
             </div>
-            <div class="map mt-6 border-2 border-gray p-2">
-              <p class="title mb-2">Gurlan filial</p>
-              <iframe
-                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d782.8639892655232!2d60.38589282923386!3d41.833589997938816!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xab8fd23d29ff77fd!2zNDHCsDUwJzAwLjkiTiA2MMKwMjMnMTEuMiJF!5e1!3m2!1sen!2s!4v1654280833786!5m2!1sen!2s"
-                height="450"
-                style="border: 0"
-                allowfullscreen=""
-                loading="lazy"
-                referrerpolicy="no-referrer-when-downgrade"
-                class="w-full"
-              ></iframe>
-            </div>
           </div>
+        </div>
+      </section>
+      <svg
+        width="254"
+        height="481"
+        viewBox="0 0 254 481"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute -left-24 bottom-52 -z-20"
+      >
+        <circle cx="54" cy="200" r="200" fill="#00B4D8" fill-opacity="0.12" />
+        <circle
+          cx="85.2345"
+          cy="410.235"
+          r="50"
+          transform="rotate(38.3471 85.2345 410.235)"
+          fill="url(#paint0_linear_109_39)"
+          
+        />
+        <defs>
+          <linearGradient
+            id="paint0_linear_109_39"
+            x1="85.2345"
+            y1="360.235"
+            x2="135.235"
+            y2="493.235"
+            gradientUnits="userSpaceOnUse"
+          >
+            <stop stop-color="#00B4D8" />
+            <stop offset="1" stop-color="#98EEFF" />
+          </linearGradient>
+        </defs>
+      </svg>
+
+      <section class="section overflow-hidden rounded mb-8">
+        <div
+          class="section-title rounded-t bg-primary text-xl px-6 py-5 text-white flex flex-row items-center"
+        >
+          <ion-icon name="trophy-outline"></ion-icon>
+          <p class="ml-3 font-bold">
+            O’quvchilarimiz uchun yaratilgan sharoitlar
+          </p>
+        </div>
+        <div
+          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-6 px-6 py-4 pb-6 text-[18px]"
+        >
+          <p>
+            Markazimizda oy yakunida turli test sinovlari, olimpiadalar
+            o’tkaziladi. Bu bilan o’quvchilar orasida raqobat ruhini
+            shakllantirish ko’zda tutilgan. Ushbu olimpiadalarda qatnashgan
+            o’quvchilar esa qimmatbaho sovg’alar, eng so’ngi nashrdagi kitoblar,
+            gadjetlar va eng asosiysi, o’quv markazimizda 1 oy bepul ta’lim
+            olish uchun grand vaucherlar bilan taqdirlanadilar 🚀
+          </p>
+          <p>
+            HDP o’quv markazi o’zining o’quvchilar uchun yaratilgan zamonaviy va
+            qulay shart-sharoitlari bilan boshqa o’quv markazlardan ajralib
+            turadi. Markaz binosida o’ziga xos dizayn bilan qurilgan
+            sinfxonalar, kutubxonalar, oshxonalar va o’quvchilar darslardan
+            tashqari mustaqil shug’ullanishlari uchun qulay sharoitlar bilan
+            openspace maydonlari ham tashkil qilingan 🔝
+          </p>
+          <p>
+            Kurslarimizni yaxshi natijalar bilan tugatgan o’quvchilarimiz
+            endilikda ish qidirib yurishiga hojat qolmaydi. Chunki markazimiz
+            ma’muriyati ularga markazda ishlashi uchun yuqori maoshli ish taklif
+            qilishadi. Shu kungacha markazimizni yaxshi natijalar bilan bitirgan
+            bir qancha o’quvchilarimiz o’zidan keyingi o’quvchilarga dars berib
+            kelmoqdalar 😊
+          </p>
+        </div>
+      </section>
+      <svg
+        width="293"
+        height="400"
+        viewBox="0 0 293 400"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        class="absolute -right-24 bottom-[640px] -z-10"
+      >
+        <circle cx="200" cy="200" r="200" fill="#00B4D8" fill-opacity="0.12" />
+      </svg>
+
+      <section class="section overflow-hidden rounded mb-8">
+        <div
+          class="section-title rounded-t bg-primary text-xl px-6 py-5 text-white flex flex-row items-center"
+        >
+          <ion-icon name="star-outline"></ion-icon>
+          <p class="ml-3 font-bold">Sifat, metodika, nazorat</p>
+        </div>
+        <div
+          class="section-body bg-white overflow-hidden rounded-b border-b-2 border-l-2 border-r-2 border-primary font-medium relative space-y-6 px-6 py-4 pb-6 text-[18px]"
+        >
+          <ul class="space-y-3">
+            <p>
+              HDP o’quv markazida darslar tizimlashtirilgan qat’iy tartib
+              asosida, haftada 5 kun 3 soatdan olib boriladi. Asosiy dars o’tish
+              sxemasi quyidagilardan iborat:
+            </p>
+            <li class="font-extrabold">
+              - Leksiya. Asosiy dars o’qituvchi tomonidan olib boriladi
+            </li>
+            <li class="font-extrabold">
+              - Seminar. Kuratorlar leksiyada o’tilgan mavzularni
+              o’quvchilarning qay darajada o’zlashtirganligini, daftarlarini
+              tekshirishadi va tushunmagan mavzularini o’rgatishadi
+            </li>
+            <li class="font-extrabold">
+              - Individual. Leksiyadan keyin yordamchi o’qituvchilar tomonidan
+              har bir o’quvchiga alohida yondashilinadi
+            </li>
+            <li class="font-extrabold">
+              - Yakshanba. Haftaning bu kunida haftalik va oylik savol-javob
+              testlari, musobaqalar va olimpiadalar bo’lib o’tadi. Shuningdek,
+              o’quvchining natijalari uning ota-onalariga SMS tarzida
+              yuboriladi. Bundan tashqari har hafta shu kuni markazimizga
+              muvafaqqiyatli insonlar tashrif buyurib, o’quvchilarga bepul
+              treninglar o’tib, ularni o’qishga, kasb tanlashga qiziqishlarini
+              uyg’otishadi
+            </li>
+          </ul>
+          <p>
+            Bundan tashqari, o’quv markazimizning rasmiy YouTube kanalida
+            o’qituvchilarimiz tomonidan fanlarning turli mavzulari haqida
+            joylangan video darsliklar ham mavjud 🔥
+          </p>
+          <p>
+            O’qituvchi va o’quvchi orasidagi munosabatini mustahkamlash, unga
+            mavzuni tushuntirish uslubini topish va stressga uchragan
+            o’quvchilarga ruhiy dalda berish uchun har oy uning ruhiy va
+            ijtimoiy holati, ota-onasi bilan munosabati va yashash sharoiti
+            o’rganiladi 🤗
+          </p>
+          <button
+            @click="openChannel()"
+            class="youtube-btn flex flex-row items-center"
+          >
+            <ion-icon class="mr-2" name="play-outline"></ion-icon>
+            Kanalga a’zo bo’lish
+          </button>
         </div>
       </section>
     </div>
@@ -137,6 +298,19 @@
 <style scoped>
 .section ion-icon {
   --ionicon-stroke-width: 38px;
+}
+
+.youtube-btn {
+  color: white;
+  padding: 20px;
+  border-radius: 8px;
+  background: linear-gradient(123.79deg, #ff1313 29.96%, #ff4d4d 113.53%);
+  transition: all 0.4s ease;
+  background-size: 200% auto;
+}
+
+.youtube-btn:hover {
+  background-position: right center;
 }
 
 .btn-grad {
@@ -150,11 +324,12 @@
 
 .btn-grad:hover {
   background-position: right center; /* change the direction of the change here */
-  color: #fff;
-  text-decoration: none;
 }
 
 @media (max-width: 456px) {
+  .youtube-btn {
+    width: 100%;
+  }
   .btn-grad {
     padding: 20px;
     font-size: 18px;
@@ -172,4 +347,7 @@
 <script setup>
 import book from "~/assets/img/book.png";
 import champ from "~/assets/img/champ.png";
+const openChannel = () => {
+  window.open("https://www.youtube.com/c/HDPOFFICIAL", "_blank");
+};
 </script>
